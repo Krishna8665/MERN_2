7098be72dad13ebebd35f12927e19050a5c55f51
Krishna8665 <khatrikrissna11@gmail.com>
2025-12-10 22:19:52 +0545

    login

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./database/connectDB");
 const User = require("./model/userModel");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 
 const bcrypt = require("bcrypt");
@@ -10,7 +12,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/", async (req, res) => {
+app.post("/register", async (req, res) => {
   const { email, password, phoneNumber, username } = req.body;
   if (!email || !password || !phoneNumber || !username) {
     return res.status(400).json({
@@ -47,6 +49,23 @@ app.post("/login", async (req, res) => {
   if (userFound.length == 0) {
     return res.status(400).json({
       message: "USer with that email is not registered",
+    });
+  }
+  //password check
+  const isMatched = bcrypt.compareSync(password, userFound[0].userPassword);
+  if (!isMatched) {
+    //generate token
+    const token = jwt.sign({ id: userFound[0]._id }, process.env.SECRET_KEY, {
+      expiresIn: "30d",
+    });
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      token,
+    });
+  } else {
+    res.status(404).json({
+      message: "Invalid Password",
     });
   }
 });
