c2e5b615bde8e381944e1ae7d10315691949bc87
Krishna8665 <khatrikrissna11@gmail.com>
2026-01-02 22:38:04 +0545

    controller

--- server/routes/adminUsersRoute.js
+++ server/routes/adminUsersRoute.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const permitTo = require("../middleware/permitTo");
-const { getUsers } = require("../controller/admin/user/userController");
+const {
+  getUsers,
+  deleteUser,
+} = require("../controller/admin/user/userController");
 
 const router = express.Router();
 
 //routes here
 
 router.route("/users").get(isAuthenticated, permitTo("admin"), getUsers);
+router.route("/users/:id").get(isAuthenticated, permitTo("admin"), deleteUser);
 
 module.exports = router;
