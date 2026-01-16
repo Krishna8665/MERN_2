0045d7de0259f33f7c25408506c6e20d521949c1
Krishna8665 <khatrikrissna11@gmail.com>
2026-01-16 13:43:15 +0545

    app.js

--- server/app.js
+++ server/app.js
@@ -13,14 +13,22 @@ app.use(express.static("uploads"));
 //const User = require("./model/userModel");
 // const jwt = require("jsonwebtoken");
 // const bcrypt = require("bcrypt");
-const { registerUser, loginUser } = require("./controller/auth/authController");
+//const { registerUser, loginUser } = require("./controller/auth/authController");
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   })
+// );
 //Routes Here
 const authRoute = require("./routes/authRoute");
 const productRoute = require("./routes/productRoute");
